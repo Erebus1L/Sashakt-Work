f63593017204920a7fe70707ad794d0727a856e9
Saumya <89197840+saumya-0510@users.noreply.github.com>
2022-04-18 19:17:35 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/sashakt/CameraPreview.kt
@@ -0,0 +1,66 @@
+package com.example.sashakt
+
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.hardware.Camera
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import java.io.IOException
+
+
+class CameraPreview(
+    context: Context,
+    private val mCamera: Camera
+) : SurfaceView(context), SurfaceHolder.Callback {
+
+    private val mHolder: SurfaceHolder = holder.apply {
+
+        addCallback(this@CameraPreview)
+
+        setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        mCamera.apply {
+            try {
+                setPreviewDisplay(holder)
+                startPreview()
+            } catch (e: IOException) {
+                Log.d(TAG, "Error setting camera preview: ${e.message}")
+            }
+        }
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, w: Int, h: Int) {
+
+        if (mHolder.surface == null) {
+
+            return
+        }
+
+
+        try {
+            mCamera.stopPreview()
+        } catch (e: Exception) {
+
+        }
+
+
+
+        // start preview with new settings
+        mCamera.apply {
+            try {
+                setPreviewDisplay(mHolder)
+                startPreview()
+            } catch (e: Exception) {
+                Log.d(TAG, "Error starting camera preview: ${e.message}")
+            }
+        }
+    }
+}
